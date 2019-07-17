7f7dcb2424e816c88304ee447de65d7119e55f78
shipeidong <shipeidong_v@didichuxing.com>
2019-07-17 19:57:58 +0800

    init project

---
+++ mongobucks/src/main/java/com/tuertu/spring/mongobucks/MongobucksApplication.java
@@ -0,0 +1,13 @@
+package com.tuertu.spring.mongobucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongobucksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongobucksApplication.class, args);
+    }
+
+}
